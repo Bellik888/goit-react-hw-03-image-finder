b7626aa4c089b6090e414007b692a5f2e384111c
Bellik888 <Bellik888@gmail.com>
2021-10-17 16:59:36 +0300

    add loader

--- src/components/ImagesList/ImagesList.js
+++ src/components/ImagesList/ImagesList.js
@@ -1,5 +1,7 @@
 import { Component } from 'react';
 
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
+import Loader from 'react-loader-spinner';
 import { PixabayAPI } from '../../service/pixabayAPI';
 import { ImageGallery } from '../ImageGallery/ImageGallery';
 import { ImageGalleryItem } from '../ImageGalleryItem/ImageGalleryItem';
@@ -71,7 +73,17 @@ export class ImagesList extends Component {
     }
 
     if (status === 'pending') {
-      return <p>Loading...</p>;
+      return (
+        <div className="loader">
+          <Loader
+            type="Circles"
+            color="#00BFFF"
+            height={100}
+            width={100}
+            timeout={3000}
+          />
+        </div>
+      );
     }
 
     if (status === 'rejected') {
